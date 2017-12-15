cd1ee493ca7221c3b4ffb694384921aec1177ebd
Israel <213withani@gmail.com>
2017-12-14 23:08:30 -0800

    Added spaces

--- todo/main.js
+++ todo/main.js
@@ -6,7 +6,6 @@
     button.addEventListener('click', populateList, false);
     todoList.addEventListener('click', deleteItem, false);
 
-
     function populateList(event) {
         // prevent form from submitting and create elements
         event.preventDefault();
@@ -14,23 +13,21 @@
         const text = document.createTextNode(input.value);
         const span = document.createElement('span');
         const x = document.createTextNode('x');
-        
+
         // set and append elements
         li.textContent = text.nodeValue;
         span.textContent = x.nodeValue;
         li.appendChild(span);
         todoList.appendChild(li);
-
     }
 
     function deleteItem(event) {
         // find the element clicked and delete its parent.
         const clicked = event.target;
-        
+
         console.log(clicked.nodeName);
         if (clicked.nodeName === 'SPAN') {
             todoList.removeChild(clicked.parentNode);
         }
-
     }
 })();
