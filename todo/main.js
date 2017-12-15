1226133ebb54ae0d4cbd7221be35297949f2259e
Israel <213withani@gmail.com>
2017-12-14 23:02:12 -0800

    Added comments

--- todo/main.js
+++ todo/main.js
@@ -8,23 +8,25 @@
 
 
     function populateList(event) {
-        console.log("populateList");
+        // prevent form from submitting and create elements
         event.preventDefault();
         const li = document.createElement('li');
         const text = document.createTextNode(input.value);
         const span = document.createElement('span');
         const x = document.createTextNode('x');
+        
+        // set and append elements
         li.textContent = text.nodeValue;
         span.textContent = x.nodeValue;
         li.appendChild(span);
-        console.log(li);
         todoList.appendChild(li);
 
     }
 
     function deleteItem(event) {
+        // find the element clicked and delete its parent.
         const clicked = event.target;
-        console.log(clicked);
+        
         console.log(clicked.nodeName);
         if (clicked.nodeName === 'SPAN') {
             todoList.removeChild(clicked.parentNode);
