3955ddfb1f94e43168959f951ec35b2c7fc6f7f0
Israel <213withani@gmail.com>
2018-01-18 19:47:38 -0800

    The user can only have 4 wrong choices.

--- memorizeGame/main.js
+++ memorizeGame/main.js
@@ -3,51 +3,38 @@
   let cellsToHighlight = null;
   let footer = document.getElementById("footer");
   let count = 0;
-  // let i = 0; 
-  
+  let activeCell = [];
+  let cellToClear = [];
   const gameTransition = {
     ready: "Ready",
-    recall: "Recall"
+    recall: "Recall",
+    over: "Game Over"
   }
-
   function Game() {
     let allCellsPositions = [];
-
-    footer.innerHTML = gameTransition.ready;
-
+    header.innerHTML="Memorize Game: You can only get 4 wrong answers.";
     allCellsPositions = createGrid();
-
+    footer.innerHTML = gameTransition.ready;
     highlightCells(allCellsPositions);
-    //if (count <= 3) {
-      setTimeout(() => clearGrid(), 1000);
-
-      footer.innerHTML = gameTransition.recall;
-    //}
+    setTimeout(() => clearGrid(), 1000);
   }
-
   Game();
-
   function createGrid() {
     let cell = null,
       grid = null;
     let allCellsPositions = [];
-
     grid = document.querySelector(".wrapper");
-
     for (let m = 0; m < totalNumberOfCells; m++) {
       cell = document.createElement("div");
       cell.setAttribute("id", `${m}`);
       allCellsPositions.push(`${m}`);
       grid.appendChild(cell);
     }
-    
     return allCellsPositions;
   }
-
   function highlightCells(allCellsPositions) {
     var cellToHighlight = null;
     cellsToHighlight = _.sampleSize(allCellsPositions, 6);
-
     for (let k = 0; k < cellsToHighlight.length; k++) {
       cellToHighlight = document.getElementById(cellsToHighlight[k]);
       cellToHighlight
@@ -55,66 +42,61 @@
         .add("active");
     }
   }
-
   function clearGrid() {
     let cellToClear = null;
-
     for (let l = 0; l < totalNumberOfCells; l++) {
       cellToClear = document.getElementById(l);
       cellToClear
         .classList
         .remove("active");
     }
-
     allowUserToClickCells();
     
+    footer.innerHTML = gameTransition.recall;
   }
-
   function allowUserToClickCells() {
-    let cellToClear = null;
-
     for (let i = 0; i < totalNumberOfCells; i++) {
-      
-      cellToClear = document.getElementById(i);
-      cellToClear.addEventListener('click', clickCorrectCell.bind(cellToClear));
-    
+      cellToClear.push(document.getElementById(i));
+      activeCell[i] = clickCorrectCell.bind(cellToClear[i], i);
+      console.log(activeCell);
+      cellToClear[i].addEventListener('click', activeCell[i]);
     }
   }
-
-  function clickCorrectCell() {
+  function clickCorrectCell(num) {
     const correctCell = isCorrectCell.bind(this);
-    correctCell(i);
+    correctCell(num);
   }
-
   function disableCells() {
-    let cellToClear = null;
-
+    console.log(activeCell);
     for (let j = 0; j < totalNumberOfCells; j++) {
-      cellToClear = document.getElementById(j);
-      cellToClear.removeEventListener("click", this.clickCorrectCell);
+      //cellToClear = document.getElementById(j);
+      console.log(cellToClear[j]);
+      cellToClear[j].removeEventListener('click', activeCell[j]);
     }
   }
-
-  function isCorrectCell(i) {
-    const cellToHighlight = i.toString();
+  function isCorrectCell(num) {
+    console.log(this);
+    const cellToHighlight = num.toString();
     for (let j = 0; j < cellsToHighlight.length; j++) {
       if (cellToHighlight === cellsToHighlight[j]) {
         this
           .classList
           .add("correct");
+        console.log("correct");
       }
     }
-    
     if (!this.classList.contains("correct")) {
       this
         .classList
         .add("wrong");
+      console.log("wrong");
       count++;
       console.log(count);
     }
-
-    if(count > 3){
-      disableCells()
+    if (count > 3) {
+      // debugger;
+      disableCells();
+      footer.innerHTML = gameTransition.over;
     }
   }
 })();
