ca17109432af61e5c6450af2f6c7cfad96f160b8
Israel <213withani@gmail.com>
2018-02-05 20:03:20 -0800

    used chart.js demo to  update options to use time

--- charts/main.js
+++ charts/main.js
@@ -30,14 +30,38 @@ const CHART = document.getElementById("lineChart");
 let lineChart = new Chart(CHART, {
   type: "line",
   data: {
-    labels: ["Jan", "feb", "mar", "april", "may", "june", "july"],
-    datasets: [
-      {
-        label: "My First dataset",
-        fill:false,
-        borderColor:'lightgreen',
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
+    labels: ["2018-02-02T05:57:04Z", "2018-07-02T05:58:04Z", "2018-010-02T05:57:04Zmar"],
+    datasets: [{
+      label: "My First dataset",
+      fill: false,
+      borderColor: 'lightgreen',
+      data: [2.375746290045407e-18, 3.827426164227489e-17, 5.827426164227489e-17]
+    }]
+  },
+  options: {
+    title: {
+      text: "Chart.js Time Scale"
+    },
+    scales: {
+      xAxes: [{
+        type: "time",
+        time: {
+          format: 'YYYY-MM-DD HH:mm',
+          // round: 'day'
+          tooltipFormat: 'll HH:mm'
+        },
+        scaleLabel: {
+          display: true,
+          labelString: 'Date'
+        }
+      }, ],
+      yAxes: [{
+        scaleLabel: {
+          display: true,
+          labelString: 'value'
+        }
+      }]
+    },
   }
+
 });
